1b25e4895868dc9d10a31a50ebf16f3fd888ebfc
feng gu <fgu30@gatech.edu>
2014-02-16 11:00:00 +0100

    client

--- server/src/api/logs.js
+++ server/src/api/logs.js
@@ -1,35 +1,32 @@
 const {
-    Router
+  Router,
 } = require('express');
 
 const LogEntry = require('../models/LogEntry');
 
 const router = Router();
 
-router.get("/", async (req, res, next) => {
-    try {
-        const entries = await LogEntry.find();
-        res.json(entries);
-
-    } catch (error) {
-        next(error);
-    }
+router.get('/', async (req, res, next) => {
+  try {
+    const entries = await LogEntry.find();
+    res.json(entries);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.post('/', async (req, res, next) => {
-    try {
-        const logEntry = new LogEntry(req.body);
-        const createEntry = await logEntry.save();
-        res.json(createEntry);
-    } catch (error) {
-        if (error === "ValidationError") {
-            res.status(422);
-        }
-        next(error);
+  try {
+    const logEntry = new LogEntry(req.body);
+    const createEntry = await logEntry.save();
+    res.json(createEntry);
+  } catch (error) {
+    if (error === 'ValidationError') {
+      res.status(422);
     }
-    console.log(req.body);
+    next(error);
+  }
+  console.log(req.body);
 });
-
-
 
 module.exports = router;
--- server/src/index.js
+++ server/src/index.js
@@ -1,44 +1,45 @@
-const express = require('express');
-const morgan = require('morgan');
-const helmet = require('helmet')
-const cors = require('cors')
-const middlewares = require('./middlewares');
-const mongoose = require('mongoose');
-const logs = require('./api/logs');
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const logs = require("./api/logs");
+const meters = require("./api/meters");
+const middlewares = require("./middlewares");
 
-
-
-require('dotenv').config();
-
+require("dotenv").config();
 
 const app = express();
 
 mongoose.connect(process.env.DATABASE_URL, {
     useUnifiedTopology: true,
-    useNewUrlParser: true
+    useNewUrlParser: true,
 });
 
-app.use(morgan('common'));
+app.use(morgan("common"));
 app.use(helmet());
-app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-}));
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN,
+    })
+);
 
 app.use(express.json()); // support json encoded bodies
 
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
     res.json({
-        status: "My API is alive!"
+        status: "My API is alive!",
     });
 });
 
 app.use("/api/logs", logs);
+app.use("/api/meters", meters);
 
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
 
-const port = process.env.PORT || 1337;
+const port = process.env.PORT || 2001;
 
 app.listen(port, () => {
-    console.log(`The server is Listening to http://localhost:${port}`)
+    console.log(`the server is listening at ${port}`);
 });
