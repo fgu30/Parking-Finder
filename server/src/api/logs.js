eb08c1c483b2a770f24141c69828026943dd9892
feng gu <fgu30@gatech.edu>
2014-02-22 11:00:00 +0100

    bugfix

--- server/src/api/logs.js
+++ server/src/api/logs.js
@@ -2,16 +2,34 @@ const {
     Router
 } = require('express');
 
+const LogEntry = require('../models/LogEntry');
+
 const router = Router();
 
-router.get("/", (req, res) => {
-    res.json({
-        message: "hello world"
-    });
+router.get("/", async (req, res, next) => {
+    try {
+        const entries = await LogEntry.find();
+        res.json(entries);
+
+    } catch (error) {
+        next(error);
+    }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res, next) => {
+    try {
+        const logEntry = new LogEntry(req.body);
+        const createEntry = await logEntry.save();
+        res.json(createEntry);
+    } catch (error) {
+        if (error === "ValidationError") {
+            res.status(422);
+        }
+        next(error);
+    }
     console.log(req.body);
 });
+
+
 
 module.exports = router;
--- server/src/models/LogEntry.js
+++ server/src/models/LogEntry.js
@@ -1,4 +1,4 @@
-  import mongoose from 'mongoose';
+  const mongoose = require('mongoose');
   const {
       Schema
   } = mongoose;
@@ -11,23 +11,18 @@
       type: Number,
       required: true
   };
-  //   const defaultRequiredDate = {
-  //       type: Date,
-  //       default: Date.now,
-  //       required: true
-  //   };
 
   const logEntrySchema = new Schema({
-      Title = requiredString,
+      Title: requiredString,
       images: String,
       description: String,
       comments: String,
-      Latitude = {
+      Latitude: {
           ...requiredNumber,
           max: 180,
           min: -180
       },
-      Longitude = {
+      Longitude: {
           ...requiredNumber,
           max: 90,
           min: -90
@@ -38,7 +33,7 @@
           max: 10,
           default: 0
       },
-      visitDate = {
+      visitDate: {
           type: Date,
           required: true
       }
