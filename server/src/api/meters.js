a3ea76d36d1544654f5d24d1f4102c2d3779e5c4
fenggu <fgu30@gatech.edu>
2021-03-22 01:02:42 -0400

    server routing

--- server/src/api/meters.js
+++ server/src/api/meters.js
@@ -5,26 +5,12 @@ const Meter = require("../models/Meter");
 const router = Router();
 
 router.get("/", async (req, res, next) => {
-    try {
-        const entries = await Meter.find();
-        res.json(entries);
-    } catch (error) {
-        next(error);
-    }
+  try {
+    const entries = await Meter.find();
+    res.json(entries);
+  } catch (error) {
+    next(error);
+  }
 });
-
-// router.post("/", async (req, res, next) => {
-//     try {
-//         const logEntry = new LogEntry(req.body);
-//         const createEntry = await logEntry.save();
-//         res.json(createEntry);
-//     } catch (error) {
-//         if (error === "ValidationError") {
-//             res.status(422);
-//         }
-//         next(error);
-//     }
-//     console.log(req.body);
-// });
 
 module.exports = router;
